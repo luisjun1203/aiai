1fe335041c8c669d810f0ed1df297cbfe8bc185c
luisjun1203 <155038870+luisjun1203@users.noreply.github.com>
2024-01-04 19:36:32 +0900

    california_housing 데이터 넣고 모델링

--- keras/keras09_1_boston.py
+++ keras/keras09_1_boston.py
@@ -7,6 +7,7 @@ from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
 from sklearn.model_selection import train_test_split
+import time
 
 # 현재 scikit-learn version 1.3.0 보스턴 안됨. 그래서 삭제함
 # pip uninstall scikit-learn 
@@ -20,16 +21,16 @@ datasets = load_boston()
 
 X = datasets.data
 y = datasets.target
-print(X)
-print(X.shape)      # (506, 13)     # 열의 개수 알고싶을때
-print(y)
-print(y.shape)      # (506, )
+# print(X)
+# print(X.shape)      # (506, 13)     # 열의 개수 알고싶을때
+# print(y)
+# print(y.shape)      # (506, )
 
-print(datasets.feature_names)       # 속성 이름 알고싶을때
+# print(datasets.feature_names)       # 속성 이름 알고싶을때
 # ['CRIM' 'ZN' 'INDUS' 'CHAS' 'NOX' 'RM' 'AGE' 'DIS' 'RAD' 'TAX' 'PTRATIO'
 #  'B' 'LSTAT']
 
-print(datasets.DESCR)               # 속성
+# print(datasets.DESCR)               # 속성
 
 # [실습]
 # train_size 0.7 이상, 0.9이하
@@ -46,7 +47,10 @@ model.add(Dense(21))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, epochs=10000, batch_size=15)
+start_time = time.time()
+model.fit(X_train, y_train, epochs=1000, batch_size=20)
+end_time = time.time()
+
 
 loss = model.evaluate(X_test, y_test)
 print("로스 : ", loss)
@@ -58,6 +62,7 @@ from sklearn.metrics import r2_score
 r2 = r2_score(y_test, y_predict)
 print("R2스코어 : ", r2)
 
+print("걸린 시간  : ", round(end_time - start_time, 2), "초")
 
 # random_state=42 epochs=1000, batch_size=15, test_size=0.15
 # 로스 :  14.557331085205078
@@ -69,6 +74,6 @@ print("R2스코어 : ", r2)
 
 
 
-
-
-
+# random_state=42, epochs=1000, batch_size=15
+# 로스 :  13.158198356628418
+# R2스코어 :  0.7985079556506842
---
+++ keras/keras09_2_california.py
@@ -0,0 +1,68 @@
+from sklearn.datasets import fetch_california_housing
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+from sklearn.model_selection import train_test_split
+import time                     # 시간 알고싶을때
+
+
+
+datasets = fetch_california_housing()
+X = datasets.data
+y = datasets.target
+
+# print(X)
+# print(y)
+# print(X.shape, y.shape)         # (20640, 8) (20640,)
+
+
+# print(datasets.feature_names)
+# ['MedInc', 'HouseAge', 'AveRooms', 'AveBedrms', 'Population', 'AveOccup', 'Latitude', 'Longitude']
+#print(datasets.DESCR)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, shuffle=True, random_state=59)
+
+# print(X_train)
+# print(X_test)
+
+model = Sequential()
+model.add(Dense(21,input_dim=8))
+model.add(Dense(7))
+model.add(Dense(18))
+model.add(Dense(12))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+start_time = time.time()
+model.fit(X_train, y_train, epochs=500, batch_size=400)
+end_time = time.time()
+
+loss = model.evaluate(X_test, y_test)
+print("로스 : ", loss)
+
+y_predict = model.predict(X_test)
+result = model.predict(X)
+
+from sklearn.metrics import r2_score
+r2 = r2_score(y_test, y_predict)
+print("R2스코어 : ", r2)
+print("걸린시간 : ", round(end_time - start_time, 3), "초")
+
+
+# epochs=10000, batch_size=80, test_size=0.15, random_state=59
+# 로스 :  0.5511764883995056
+# R2스코어 :  0.6172541292238007
+
+
+
+
+
+
+
+
+
+
+
+
+
