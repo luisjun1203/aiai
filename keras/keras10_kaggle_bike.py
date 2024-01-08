d6662e5a1ef645145ba55329bf340cf6de040269
luisjun1203 <155038870+luisjun1203@users.noreply.github.com>
2024-01-08 16:01:35 +0900

    RMSE, RMSLE 보는법

--- keras/keras10_kaggle_bike.py
+++ keras/keras10_kaggle_bike.py
@@ -3,8 +3,10 @@ import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_squared_error
+from sklearn.metrics import r2_score, mean_squared_log_error, mean_squared_error
 import time
+
+
 
 path = "c:\\_data\\kaggle\\bike\\"
 
@@ -27,15 +29,22 @@ print(y.shape)     # (10886, 8)
 # df = pd.DataFrame(train_csv, columns = ['casual', 'registered', 'count'])
 # list(df['count']>0)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.11, shuffle=True, random_state=41513322)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=True, random_state=713)
 
 # print(X_train.shape, y_train.shape)     # (9253, 8) (9253,11)
 # print(X_test.shape, y_test.shape)       # (1633, 8) (1633, 11)
 
 
 model = Sequential()
-model.add(Dense(16, input_dim = 8, activation='relu'))                 # activation : 활성화함수, default : linear
+model.add(Dense(16, input_dim = 8, activation='relu'))                 # activation : 활성화함수, default : linear, activation : 하이퍼 파라미터
 model.add(Dense(32, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(64, activation='relu'))
 model.add(Dense(64, activation='relu'))
 model.add(Dense(16, activation='relu'))
 model.add(Dense(4, activation='relu'))
@@ -57,11 +66,26 @@ print(y_submit)
 
 submission_csv['count'] = y_submit                                        
 print(submission_csv)
-print("로스 : ", loss)
+print("mse : ", loss)
 # print("R2스코어 : ", r2)
 submission_csv.to_csv(path + "submission_0108_10_2.csv", index=False)
 
 print("음수갯수 : ",submission_csv[submission_csv['count']<0].count())      # 0보다 작은 조건의 모든 데이터셋을 세줘
+
+y_predict = model.predict(X_test)                                           #rmse 구하기
+# def RMSLE(y_test, y_predict):
+#     np.sqrt(mean_squared_log_error(y_test, y_predict))
+#     return np.sqrt(mean_squared_log_error(y_test, y_predict))
+# rmsle = RMSLE(y_test, y_predict)
+
+def RMSE(y_test, y_predict):
+    np.sqrt(mean_squared_error(y_test, y_predict))
+    return np.sqrt(mean_squared_error(y_test, y_predict))
+rmse = RMSE(y_test, y_predict)
+# print("256255음수갯수 : ",submission_csv[submission_csv['count']<0].count())      # 0보다 작은 조건의 모든 데이터셋을 세줘
+
+# print("RMSLE : ", rmsle)
+print("RMSE : ", rmse)
 
 
 
