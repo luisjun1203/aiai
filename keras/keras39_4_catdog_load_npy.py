6cb4033f9c1793fb47136cef74e77eadc96a98c3
luisjun1203 <155038870+luisjun1203@users.noreply.github.com>
2024-01-24 18:53:19 +0900

    파일 변환 후 제출해보기
    복습하자 어렵다

--- keras/keras39_4_catdog_load_npy.py
+++ keras/keras39_4_catdog_load_npy.py
@@ -10,6 +10,10 @@ from sklearn.metrics import f1_score, accuracy_score
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, LabelEncoder, OneHotEncoder
 import time
 import matplotlib.pyplot as plt
+import os
+
+
+
 
 # xy_train = train_datagen.flow_from_directory(
 #     path_train, 
@@ -18,7 +22,7 @@ import matplotlib.pyplot as plt
 #     class_mode='binary',
 #     shuffle=True)
 
-
+path = "c:\\_data\\image\\cat_and_dog\\test\\"
 
 np_path = "c:\\_data\\_save_npy\\"
 
@@ -26,14 +30,17 @@ np_path = "c:\\_data\\_save_npy\\"
 # np.save(np_path + 'keras37_1_y_train.npy', arr=y)  
 
 
-X_train = np.load(np_path + 'keras37_3_X_train.npy')
-y_train = np.load(np_path + 'keras37_3_y_train.npy')
-X_test = np.load(np_path + 'keras37_3_X_test.npy')
-y_test = np.load(np_path + 'keras37_3_y_test.npy')
+X = np.load(np_path + 'keras37_3_X_train.npy')
+y = np.load(np_path + 'keras37_3_y_train.npy')
+test = np.load(np_path + 'keras37_3_test1.npy')
+
 
 # print(X.shape)      # (19996, 150, 150, 3)
 # print(y.shape)      # (19996,)
-# X_train, X_test, y_train, y_test = train_test_split(X , y, test_size=0.15, shuffle=True, random_state=3, stratify=y)
+# print(test.shape)   # (5000, 100, 100, 3)
+
+X_train, X_test, y_train, y_test = train_test_split(X , y, test_size=0.15, shuffle=True, random_state=3, stratify=y)
+ 
 
 
 
@@ -50,33 +57,61 @@ model.add(Flatten())
 model.add(Dense(8,activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
-model.summary()
+# model.summary()
 
 # 3. 컴파일, 훈련
 
 # strat_time = time.time()
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
-es = EarlyStopping(monitor='val_loss', mode='min', patience=10, verbose=2, restore_best_weights=True)
-model.fit(X_train, y_train, epochs=20, batch_size=10, verbose=2, validation_split=0.15, callbacks=[es])
+es = EarlyStopping(monitor='val_loss', mode='min', patience=30, verbose=2, restore_best_weights=True)
+model.fit(X_train, y_train, epochs=100, batch_size=10, verbose=2, callbacks=[es], validation_split=0.15)
 # end_time = time.time()
 # end_time = time.time()
 
 # 4. 평가, 훈련
 loss = model.evaluate(X_test, y_test)
 
-y_predict = model.predict(X_test)
-y_predict = y_predict.round()
-
+y_predict = model.predict(test)
+y_predict = np.around(y_predict.reshape(-1))
 print(y_predict)
 print(y_predict.shape)
 
 
 
+
+
+forder_dir = path + "test1"
+id_list = os.listdir(forder_dir)                    # listdir :  test1폴더에 있는 모든 파일 및 폴더의 목록을 가져온다
+for i, id in enumerate(id_list):                    # enumerate : 
+    id_list[i] = int(id.split('.')[0])
+    
+# id.split('.'): 파일 이름에서 확장자를 제거합니다. 예를 들어, '123.jpg'의 경우 '123'만 추출됩니다.
+# int(id.split('.')[0]): 추출된 문자열을 정수로 변환합니다.
+# id_list[i] = ...: 변환된 정수를 원래 목록에 다시 할당합니다. 이렇게 하면 파일 이름의 숫자 부분만 남게 됩니다.
+
+
+for id in id_list:
+    print(id)
+
+
+y_submit = pd.DataFrame({'id':id_list, 'Target':y_predict})     # 'id' : 이미지 파일의 숫자 부분을 나타냅니다. 'Target': 모델에서 예측한 결과인 y_predict를 나타냅니다.
+print(y_submit)
+print(y_submit.shape)
+y_submit.to_csv(path + "submission_01_24_1.csv", index=False)
+
+# def ACC(aaa, bbb):
+#     (accuracy_score(aaa, bbb))
+#     return (accuracy_score(aaa, bbb))
+# acc = ACC(y_test, y_predict)
+
+
+
+# print("ACC : ", acc)
+
 # print("걸린시간 : ", round(end_time-start_time, 2),"초")
 
 print("로스 : ", loss[0])
 print("acc : ", loss[1])
-
 
 
 
