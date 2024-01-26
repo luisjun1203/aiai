77e2ebe3bc4e87ad59ba70da849dcb40424c09cb
luisjun1203 <155038870+luisjun1203@users.noreply.github.com>
2024-01-26 21:31:21 +0900

    파이썬 2장 공부

---
+++ python/02장.py
@@ -0,0 +1,58 @@
+# # a =0o177
+# a = 7//3
+# print(a)
+
+# print("""asdasdasd""")
+# aaa = "eff ffff %daaa fff ffs" %3
+# print(aaa[9:12])
+
+# a ="I eat {0} apples".format(3)
+# print(a)
+
+# a = [1,2,3,['q','w','e']]
+# print(a[-1][1])
+
+# a = [1,2,3,4,5]
+# print(a[1:3])
+
+# s1 = set([1,2,3,4,5])
+# s2 = set([3,4,5,6,7])
+# print(s1 | s2)
+
+# a = True
+# b = False
+# print(type(a))
+
+# a = [1,2,3,4,]
+# # while a:
+# #     print(a.pop())
+
+# # if [1,2,3]:
+# #     print('참')
+# # else:
+# #     print('거짓')
+# print(id(a))
+
+# from copy import copy
+
+# b = a.copy()
+# print(b is a)
+# print((80+75+55)/3)
+
+# print(13%2
+# )
+# a = dict()
+# print(a[[1]])
+
+
+
+
+
+
+
+
+
+
+
+
+
