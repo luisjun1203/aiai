88e429325c48a4ea5fd578e0464bab91f63d1851
luisjun1203 <155038870+luisjun1203@users.noreply.github.com>
2024-01-30 14:41:50 +0900

    지금까지 해온 실습데이터에 LSTM적용해보기

---
+++ keras/keras49_02_california_LSTM.py
@@ -0,0 +1,99 @@
+from sklearn.datasets import fetch_california_housing
+from keras.models import Sequential, Model
+from keras.layers import Dense, Conv2D, Flatten, Dropout, GlobalAveragePooling2D, MaxPooling2D,LSTM
+import numpy as np
+from sklearn.model_selection import train_test_split
+import time
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, StandardScaler, RobustScaler
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+datasets = fetch_california_housing()
+X = datasets.data
+y = datasets.target
+
+# print(X.shape)  # (20640, 8)
+
+X = X.reshape(20640, 4, 2)
+print(X.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, shuffle=True, random_state=33)
+
+# mas = MaxAbsScaler()
+# mas.fit(X_train)
+# X_train = mas.transform(X_train)
+# X_test = mas.transform(X_test)
+
+
+
+# model = Sequential()
+# model.add(Dense(21,input_dim=8))
+# model.add(Dense(7))
+# model.add(Dense(18))
+# # model.add(Dropout(0.5))
+# model.add(Dense(12))
+# model.add(Dense(1))
+
+# i1 = Input(shape = (8,))
+# d1 = Dense(21)(i1)      
+# d2 = Dense(7)(d1)
+# d3 = Dense(18)(d2)
+# drop1 = Dropout(0.5)(d3)
+# d4 = Dense(12)(d3)
+# o1 = Dense(1)(drop1)
+# model = Model(inputs = i1, outputs = o1)
+
+model = Sequential()
+model.add(LSTM(19, return_sequences=True,          
+               input_length = 4, input_dim = 2, activation='relu'))         
+model.add(LSTM(97, ))                                                       
+model.add(Dense(9, activation='swish'))
+model.add(Dense(21, activation='swish'))
+model.add(Dense(16, activation='swish'))
+model.add(Dense(21, activation='swish'))
+model.add(Dense(1))
+model.summary()
+
+
+import datetime
+date = datetime.datetime.now()
+print(date)                     # 2024-01-17 10:52:59.857389
+date = date.strftime("%m%d_%H%M")                   # _는 str      
+print(date)                     # 0117_1058
+print(type(date))               # <class 'str'>
+
+path = "..\\_data\\_save\\MCP\\"
+filename = '{epoch:05d}-{val_loss:.4f}-{loss:.4f}.hdf5'            # 04d : 4자리 정수표현, 4f : 소수4번째자리까지 표현, 예) 1000_0.3333.hdf5
+filepath = "".join([path, 'k28_02_california_',date,'_', filename])
+                        
+# mcp = ModelCheckpoint(monitor='val_loss', mode='min', verbose=1, save_best_only=True, filepath=filepath)    
+model.compile(loss='mse', optimizer='adam')                                                             # early stopping 개념, min,max, auto
+es = EarlyStopping(monitor='val_loss', mode='min',patience=100, verbose= 1, restore_best_weights=True) 
+hist = model.fit(X_train, y_train, epochs=200, batch_size=400, validation_split=0.2, callbacks=[es])
+# end_time = time.time()
+
+loss = model.evaluate(X_test, y_test)
+print("로스 : ", loss)
+
+y_predict = model.predict(X_test)
+result = model.predict(X)
+
+from sklearn.metrics import r2_score, mean_squared_error
+r2 = r2_score(y_test, y_predict)
+def RMSE(aaa, bbb):
+    np.sqrt(mean_squared_error(aaa, bbb))
+    return np.sqrt(mean_squared_error(aaa, bbb))
+rmse = RMSE(y_test, y_predict)
+
+print("R2스코어 : ", r2)
+# print("걸린시간 : ", round(end_time - start_time, 3), "초")
+print("RMSE : ", rmse)
+
+# 로스 :  0.5245388150215149
+# R2스코어 :  0.6191872009775989
+# RMSE :  0.7242504869844772
+
+#RNN
+# 로스 :  0.40590542554855347
+# R2스코어 :  0.7053144620181305
+# RMSE :  0.6371070914286465
---
+++ keras/keras49_07_dacon_diabetes.LSTM.py
@@ -0,0 +1,172 @@
+
+import numpy as np
+import pandas as pd
+from keras.models import Sequential, Model
+from keras.layers import Dense, Conv2D, Flatten, Dropout, GlobalAveragePooling2D, MaxPooling2D,LSTM
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score, mean_squared_error,accuracy_score
+from keras. callbacks import EarlyStopping,ModelCheckpoint
+from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, StandardScaler, RobustScaler
+path = "c://_data//dacon//diabetes//"
+
+train_csv = pd.read_csv(path + "train.csv", index_col=0)
+# print(train_csv)        #(652, 9)
+
+test_csv = pd.read_csv(path + "test.csv", index_col=0)
+# print(test_csv)         #(116, 8)
+
+submission_csv = pd.read_csv(path + "sample_submission.csv")
+# print(submission_csv)   #(116, 2)
+
+# train_csv['Glucose']
+# train_csv['Glucose'] = train_csv['Glucose'].fillna(train_csv['Glucose'].mean())
+
+# print(train_csv['Glucose'][train_csv['Glucose']==0].count())    # 4
+# print(train_csv['BloodPressure'][train_csv['BloodPressure']==0].count())    # 30
+# print(train_csv['SkinThickness'][train_csv['SkinThickness']==0].count())    # 195
+# print(train_csv['Insulin'][train_csv['Insulin']==0].count())    # 318
+# print(train_csv['BMI'][train_csv['BMI']==0].count())    # 7
+
+
+
+
+
+
+X = train_csv.drop(['Outcome'], axis=1)
+y = train_csv['Outcome']
+
+X = X.values.reshape(652, 4,2)
+test_csv = test_csv.values.reshape(116,4,2)
+
+# print(X.shape, y.shape)     # (652, 8) (652)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, shuffle=True, random_state=713)
+
+################    MinMaxScaler    ##############################
+# mms = MinMaxScaler()
+# mms.fit(X_train)
+# X_train = mms.transform(X_train)
+# X_test = mms.transform(X_test)
+# test_csv =mms.transform(test_csv)
+
+################    StandardScaler    ##############################
+
+# sts = StandardScaler()
+# sts.fit(X_train)
+# X_train = sts.transform(X_train)
+# X_test = sts.transform(X_test)
+
+# print(X_train)
+# print(X_test)
+
+# ################    MaxAbsScaler    ##############################
+# mas = MaxAbsScaler()
+# mas.fit(X_train)
+# X_train = mas.transform(X_train)
+# X_test = mas.transform(X_test)
+# test_csv = mas.transform(test_csv)
+
+# ################    RobustScaler    ##############################
+# rbs = RobustScaler()
+# rbs.fit(X_train)
+# X_train = rbs.transform(X_train)
+# X_test = rbs.transform(X_test)
+
+# print(X_train.shape, y_train.shape)         # (456, 8) (456,)
+# print(X_test.shape, y_test.shape)           # (196, 8) (196,)
+
+# model = Sequential()
+# model.add(Dense(19, input_dim = 8,activation='relu'))               
+# model.add(Dense(97))
+# model.add(Dense(9))
+# model.add(Dense(21))
+# model.add(Dense(3))
+# model.add(Dense(41))
+# model.add(Dropout(0.4))
+# model.add(Dense(1, activation='sigmoid'))
+
+# i1 = Input(shape = (8,))
+# d1 = Dense(19)(i1)      
+# d2 = Dense(97)(d1)
+# d3 = Dense(9)(d2)
+# d4 = Dense(21)(d3)
+# d5 = Dense(3)(d4)
+# d6 = Dense(41)(d5)
+# drop1 = Dropout(0.4)(d6)
+# o1 = Dense(1,activation='sigmoid')(drop1)
+# model = Model(inputs = i1, outputs = o1)
+
+
+model = Sequential()
+model.add(LSTM(19, return_sequences=True,          
+               input_length = 4, input_dim = 2, activation='relu'))         
+model.add(LSTM(97, ))                                                       
+model.add(Dense(9, activation='swish'))
+model.add(Dense(21, activation='swish'))
+model.add(Dense(16, activation='swish'))
+model.add(Dense(21, activation='swish'))
+model.add(Dense(1,activation='sigmoid'))
+model.summary()
+
+
+
+import datetime
+date = datetime.datetime.now()
+# print(date)                     # 2024-01-17 10:52:59.857389
+date = date.strftime("%m%d_%H%M")                   # _는 str      
+# print(date)                     # 0117_1058
+# print(type(date))               # <class 'str'>
+
+path2 = "..\\_data\\_save\\MCP\\"
+filename = '{epoch:05d}-{acc:.4f}-{loss:.4f}.hdf5'            # 04d : 4자리 정수표현, 4f : 소수4번째자리까지 표현, 예) 1000_0.3333.hdf5
+filepath = "".join([path2, 'k28_07_dacon_diabetes_',date,'_', filename])
+
+# mcp = ModelCheckpoint(monitor='val_loss', mode='min', verbose=1, save_best_only=True, filepath=filepath)    
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics='acc')
+es = EarlyStopping(monitor='val_loss' , mode='min', patience=200, verbose=3, restore_best_weights=True)
+hist = model.fit(X_train, y_train, epochs=1000, batch_size=32, validation_split=0.15, callbacks=[es] )
+
+loss = model.evaluate(X_test, y_test)
+y_submit = model.predict(test_csv)
+
+# print(y_submit)
+# print(y_submit.shape)       #(196, 1)
+
+submission_csv['Outcome'] = y_submit.round()                                       
+# print(submission_csv)       #(116, 2)
+
+submission_csv.to_csv(path + "submission_0130_1_.csv", index=False)
+
+y_predict = model.predict(X_test)
+y_predict = y_predict.round()
+def ACC(aaa, bbb):
+    (accuracy_score(aaa, bbb))
+    return (accuracy_score(aaa, bbb))
+acc = ACC(y_test, y_predict)
+print("ACC : ", acc)
+print("로스 : ", loss)
+
+
+
+# MinMaxScaler
+# ACC :  0.8163265306122449
+# 로스 :  [0.452159583568573, 0.8163265585899353]
+
+# # MaxAbsScaler
+# ACC :  0.8214285714285714
+# 로스 :  [0.46231532096862793, 0.8214285969734192]
+# # StandardScaler
+# ACC :  0.8061224489795918
+# 로스 :  [0.45180371403694153, 0.8061224222183228]
+
+# # RobustScaler
+# ACC :  0.8061224489795918
+# 로스 :  [0.45696309208869934, 0.8061224222183228]
+
+#cnn
+# ACC :  0.7791411042944786
+# 로스 :  [0.49091923236846924, 0.7791411280632019]
+
+#rnn
+# ACC :  0.8220858895705522
+# 로스 :  [0.4102599620819092, 0.8220859169960022]
