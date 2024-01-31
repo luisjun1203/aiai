14a7fa4f73120e97940783979d2b2cd2ea7401f9
luisjun1203 <155038870+luisjun1203@users.noreply.github.com>
2024-01-31 14:12:35 +0900

    시계열 데이터를 conv1D로 변환하여 적용해보기

---
+++ keras/keras53_Conv1D_1.py
@@ -0,0 +1,58 @@
+import numpy as np
+from keras.models import Sequential, Model
+from keras.layers import Dense,Dropout, Conv2D,SimpleRNN, LSTM, Conv1D, Flatten
+from keras.callbacks import EarlyStopping
+# 1. 데이터
+
+datasets = np.array([1,2,3,4,5,6,7,8,9,10])
+
+
+X = np.array([[1,2,3],
+             [2,3,4],
+             [3,4,5],
+             [4,5,6],
+             [5,6,7],
+             [6,7,8],
+             [7,8,9],
+             ])
+
+y = np.array([4,5,6,7,8,9,10,])
+
+
+print(X.shape)      # (7, 3)
+print(y.shape)      # (7, )
+
+X = X.reshape(-1, 3, 1)
+print(X.shape)      # (7, 3, 1)
+
+model = Sequential()
+# model.add(SimpleRNN(units = 19, input_shape = (3, 1), activation='tanh'))              # input_shape : 3-D tensor with shape (batch_size, timesteps, features)
+
+
+model.add(Conv1D(filters=10, kernel_size= 3, input_shape = (3, 1), activation='swish'))              # input_shape : 3-D tensor with shape (batch_size, timesteps, features)
+model.add(Dense(97,activation='relu'))
+model.add(Flatten())
+# model.add(Dense(7, activation='relu'))
+model.add(Dense(9,activation='relu'))
+model.add(Dense(21,activation='swish'))
+model.add(Dense(16,activation='relu'))
+model.add(Dense(21,activation='relu'))
+model.add(Dense(1))
+model.summary()
+
+#LSTM 546
+
+
+model.compile(loss = 'mse', optimizer='adam')
+model.fit(X, y, epochs=10000)
+
+results = model.evaluate(X,y)
+print("loss : ", results)
+X_predict = np.array([8,9,10]).reshape(1, 3, 1)
+y_predict = model.predict(X_predict)
+print("[8,9,10]의 예측값 : ", y_predict)
+
+# [8,9,10]의 예측값 :  [[10.909407]]
+    
+# loss :  4.4430441548115596e-09
+# [8,9,10]의 예측값 :  [[10.998966]]
---
+++ keras/keras53_Conv1D_2.py
@@ -0,0 +1,52 @@
+import numpy as np
+from keras.models import Sequential, Model
+from keras.layers import Dense,Dropout, Conv2D,SimpleRNN, LSTM, Conv1D, Flatten
+from keras.callbacks import EarlyStopping
+
+
+
+# 1. 데이터
+X = np.array([[1,2,3], [2,3,4], [3,4,5], [4,5,6],
+              [5,6,7], [6,7,8], [7,8,9], [8,9,10],
+              [9,10,11], [10,11,12],
+              [20,30,40], [30,40,50], [40,50,60],
+              ])
+y = np.array([4,5,6,7,8,9,10,11,12,13,50,60,70])
+
+X = X.reshape(-1, 3, 1)
+print(X.shape) #(13,3,1)
+
+
+model = Sequential()
+# model.add(LSTM(19, return_sequences=True,           # return_sequences : 2번이상 RNN을 쓸 수 있게 해준다. timesteps를 살려서 다음 레이어로 보내준다.
+#                input_length = 3, input_dim = 1, activation='relu'))
+# model.add(LSTM(97, ))
+model.add(Conv1D(filters=19, kernel_size=3, input_shape= (3,1)))
+model.add(Flatten())
+model.add(Dense(9, activation='swish'))
+model.add(Dense(21, activation='swish'))
+model.add(Dense(16, activation='swish'))
+model.add(Dense(21, activation='swish'))
+model.add(Dense(1))
+model.summary()
+
+
+model.compile(loss='mse', optimizer='adam')
+es = EarlyStopping(monitor='loss', mode=min, patience=100, verbose=1)
+model.fit(X, y, epochs=1000, callbacks=[es])
+
+
+results = model.evaluate(X,y)
+print("loss : ", results)
+
+X_predict = np.array([50,60,70]).reshape(-1,3,1)
+y_predict = model.predict(X_predict)
+print("[50,60,70]의 예측값 : ", y_predict)
+
+
+# [50,60,70]의 예측값 :  [[80.010216]]
+
+
+# loss :  0.0026630936190485954
+# [50,60,70]의 예측값 :  [[80.29654]]
+
