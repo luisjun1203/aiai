249db9d27ec55973f7367c852ffc3fa4399af256
luisjun1203 <155038870+luisjun1203@users.noreply.github.com>
2024-02-06 17:50:47 +0900

    초반 시드값 고정

--- keras/keras60_random_seed.py
+++ keras/keras60_random_seed.py
@@ -3,9 +3,10 @@ from keras.layers import Dense
 import tensorflow as tf
 import keras
 import numpy as np
-tf.random.set_seed(3)
-np.random.seed(3)
-
+import random as rn
+rn.seed(333)
+tf.random.set_seed(132)       #텐서 2.9 에서는 먹히지만, 2.12에서는 안먹힘
+np.random.seed(321)
 
 
 print(keras.__version__)    # 2.9.0
@@ -19,11 +20,9 @@ y = np.array([1,2,3])
 
 # 2. 모델
 model = Sequential()
-model.add(Dense(19, input_dim=1)) 
-model.add(Dense(97, input_dim=1)) 
-model.add(Dense(9, input_dim=1)) 
-model.add(Dense(21, input_dim=1)) 
-model.add(Dense(1, input_dim=1)) 
+model.add(Dense(5, input_dim=1)) 
+model.add(Dense(5)) 
+model.add(Dense(1)) 
 
 # 3. 컴파일, 훈련
 model.compile(loss='mse', optimizer='adam')
