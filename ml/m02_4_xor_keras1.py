fb37a8d0d721d17335ec1bcaa9adc5be428e469a
luisjun1203 <155038870+luisjun1203@users.noreply.github.com>
2024-02-07 14:49:44 +0900

    1차 인공지능 겨울 알아보기

---
+++ ml/m02_4_xor_keras1.py
@@ -0,0 +1,53 @@
+import numpy as np
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+from keras.models import Sequential
+from keras.layers import Dense
+
+
+
+
+
+
+
+# 1.데이터
+
+X_data = np.array([[0,0], [0,1], [1,0], [1,1]])
+y_data = np.array([0,1,1,0])
+print(X_data.shape, y_data.shape)   # (4, 2) (4,)
+
+# 2. 모델구성
+# model = LinearSVC(C=10000000000)
+# model = Perceptron()
+model = Sequential()
+
+model.add(Dense(1, activation='sigmoid', input_shape=(2,)))
+
+
+
+# 3. 훈련
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+model.fit(X_data, y_data, batch_size=1, epochs=100)
+
+# 4. 평가, 예측
+
+results = model.evaluate(X_data, y_data)
+# print("loss : ", results[0])
+print("acc : ", results[1])
+
+y_predict = model.predict(X_data)
+y_predict = np.round(y_predict).reshape(-1,).astype(int)
+acc2 = accuracy_score(y_data, y_predict)
+print("accuracy_score : ", acc2)
+print(y_data)
+print(y_predict)
+
+# model.score :  1.0
+# accuracy_score :  1.0
+# [0 1 1 1]
+# [0 1 1 1]
+
+# accuracy_score :  1.0
+# [0 1 1 0]
+# [0 1 1 0]
