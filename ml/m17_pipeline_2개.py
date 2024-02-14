a5e9532b8786ad5d3581900881dd18518fc1781a
luisjun1203 <155038870+luisjun1203@users.noreply.github.com>
2024-02-14 09:39:10 +0900

    pipeline 학습

---
+++ ml/m17_pipeline_2개.py
@@ -0,0 +1,47 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn.pipeline import make_pipeline
+from sklearn.decomposition import PCA       # 차원 축소, 주성분 분석
+
+
+datasets = load_iris()
+X = datasets.data
+y = datasets.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y )
+
+# scaler = MinMaxScaler()
+# X_train = scaler.fit_transform(X_train)
+# X_test = scaler.transform(X_test)
+# model = RandomForestClassifier()
+
+model = make_pipeline(MinMaxScaler(), StandardScaler(), PCA(), RandomForestClassifier(min_samples_split=2, min_samples_leaf=10, random_state=42))
+
+
+
+model.fit(X_train,y_train)
+
+results = model.score(X_test, y_test)
+
+print("model : ", model, ", ",'score : ', results)
+y_pred = model.predict(X_test)
+
+acc = accuracy_score(y_test, y_pred)
+print("model : ", model, ", ","acc : ", acc)
+print("\n")
+
+
+# model :  Pipeline(steps=[('minmaxscaler', MinMaxScaler()),
+#                 ('standardscaler', StandardScaler()), ('pca', PCA()),
+#                 ('randomforestclassifier',
+#                  RandomForestClassifier(min_samples_leaf=10, random_state=42))]) ,  score :  0.9333333333333333
+# model :  Pipeline(steps=[('minmaxscaler', MinMaxScaler()),
+#                 ('standardscaler', StandardScaler()), ('pca', PCA()),
+#                 ('randomforestclassifier',
+#                  RandomForestClassifier(min_samples_leaf=10, random_state=42))]) ,  acc :  0.9333333333333333
+
+
+
---
+++ ml/m18_Pipeline.py
@@ -0,0 +1,45 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+from sklearn.pipeline import make_pipeline, Pipeline
+import numpy as np
+from sklearn.metrics import accuracy_score
+import random
+random.seed(42)
+np.random.seed(42)
+
+#1 . 데이터
+X, y = load_iris(return_X_y=True)
+print(X.shape, y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=123, train_size=0.8, stratify=y)
+
+# print(np.mean(X_train), np.max(X_train))    #3.454583333333333 7.9
+# print(np.mean(X_test), np.max(X_test))      #3.504166666666667 7.7
+# scaler = MinMaxScaler()
+# X_train = scaler.fit_transform(X_train)
+# X_test = scaler.transform(X_test)
+
+#2 모델
+# model = RandomForestClassifier()
+model = make_pipeline(MinMaxScaler(), RandomForestClassifier(min_samples_split=2))
+model = Pipeline([("MinMax", MinMaxScaler())
+                  , ("RF", RandomForestClassifier())])
+  
+#3 훈련
+model.fit(X_train, y_train)
+
+#4 평가
+results = model.score(X_test, y_test)
+print('model : ', " acc :", results)
+y_pred = model.predict(X_test)
+
+acc = accuracy_score(y_test, y_pred)
+print("model : ", model, ", ","acc : ", acc)
+
+# model :   acc : 0.9666666666666667
+
+# model :   acc : 0.9666666666666667
+# model :  Pipeline(steps=[('MinMax', MinMaxScaler()), ('RF', RandomForestClassifier())]) ,  acc :  0.9666666666666667
