20bf78fd344d702ed62178b34156f25e838597ce
luisjun1203 <155038870+luisjun1203@users.noreply.github.com>
2024-03-09 09:30:51 +0900

    bagging 실습

---
+++ ml/4주차/m46_bagging02_digits.py
@@ -0,0 +1,57 @@
+import numpy as np
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from xgboost import XGBClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier, BaggingClassifier          # Bootstrap Aggregating
+from sklearn.linear_model import LogisticRegression
+import warnings
+
+warnings.filterwarnings('ignore')
+
+# 1. 데이터
+X, y = load_digits(return_X_y=True)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=777, train_size=0.8, stratify=y)
+
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+parameters = {
+    # 'objective': 'binary:logistic',  # 분류 문제인 경우 이진 분류를 위해 'binary:logistic'으로 설정합니다.
+    # 'eval_metric': 'logloss',  # 모델 평가 지표로 로그 손실을 사용합니다.
+    'max_depth': 6,  # 트리의 최대 깊이를 설정합니다.
+    'learning_rate': 0.1,  # 학습률을 설정합니다.
+    'n_estimators': 100,  # 트리의 개수를 설정합니다.
+    'subsample': 0.8,  # 각 트리마다 사용될 샘플의 비율을 설정합니다.
+    'colsample_bytree': 0.8,  # 각 트리마다 사용될 피처의 비율을 설정합니다.
+    'reg_alpha': 0,  # L1 정규화 파라미터를 설정합니다.
+    'reg_lambda': 1,  # L2 정규화 파라미터를 설정합니다.
+    'random_state': 42  # 랜덤 시드를 설정합니다.
+}
+
+# 2. 모델
+model = BaggingClassifier(LogisticRegression(),             # 하나의 모델을 사용하여 분산시켜 모델을 병렬로 만들어 처리
+                          n_estimators=10, # 디폴트         # 서로다른 종류의 모델은 결합 못한다...
+                          n_jobs=-2,
+                          random_state=777,
+                          bootstrap=True,   # 디폴트 중복을 허용한다
+                        #   bootstrap=False # 중복 허용 X
+                                                      
+                          )
+
+# 3. 훈련
+model.fit(X_train, y_train)
+
+# 4. 평가, 예측
+results = model.score(X_test, y_test)
+print("최종 점수 : ", results)
+
+y_predict = model.predict(X_test)
+acc = accuracy_score(y_test, y_predict)
+print("acc_score : ", acc)
+
+# 최종 점수 :  0.9666666666666667
+# acc_score :  0.9666666666666667
