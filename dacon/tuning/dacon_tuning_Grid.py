ac57613c804824109e0629e21ede4311964babfc
luisjun1203 <155038870+luisjun1203@users.noreply.github.com>
2024-04-01 17:03:01 +0900

    데이콘 수정(24/04/01)

--- dacon/tuning/dacon_tuning_Grid.py
+++ dacon/tuning/dacon_tuning_Grid.py
@@ -18,7 +18,7 @@ n_splits= 7
 kfold = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
 # 데이터 스케일링
 scaler = StandardScaler()
@@ -67,7 +67,7 @@ for param, value in best_params.items():
     if param in submission_csv.columns:
         submission_csv[param] = value
 
-submission_csv.to_csv(path + "sample_submission_03_28_3_.csv", index=False)
+submission_csv.to_csv(path + "sample_submission_04_01_4_.csv", index=False)
 
 
 
--- tf114/tf16_01_cancer.py
+++ tf114/tf16_01_cancer.py
@@ -1,83 +1,64 @@
-from sklearn.datasets import load_breast_cancer
-from sklearn.preprocessing import MinMaxScaler, MaxAbsScaler, StandardScaler, RobustScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_squared_error,mean_squared_log_error,accuracy_score
-import numpy as np
+from sklearn.datasets import load_iris , load_breast_cancer
 import tensorflow as tf
-tf.compat.v1.set_random_seed(3)
+import numpy as np
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler , MinMaxScaler , MaxAbsScaler
+tf.compat.v1.set_random_seed(300)
 
-datasets= load_breast_cancer()
-
-
+#1. 데이터
+datasets = load_breast_cancer()
 X = datasets.data
 y = datasets.target
 
-# print(X.shape, y.shape)     # (569, 30) (569,)
+scaler = MaxAbsScaler()
+X = scaler.fit_transform(X)
 
+# X , y  = datasets.data , datasets.target  # 위에랑 똑같다
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=3)
+print(X,X.shape)        # (569, 30)
 
-sclaer = MinMaxScaler()
-sclaer.fit(X_train)
-X_train = sclaer.transform(X_train)
-X_test = sclaer.transform(X_test)
+y = y.reshape(-1,1)     # (569, 1)
 
+print(y,y.shape)
 
-Xp = tf.compat.v1.placeholder(tf.float32, shape=[None, 30])  # 수정
-yp = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])      # 수정
+Xp = tf.compat.v1.placeholder(dtype=tf.float32 , shape=[None,30])
+yp = tf.compat.v1.placeholder(dtype=tf.float32 , shape=[None,1])
 
-w = tf.compat.v1.Variable(tf.compat.v1.random_normal([30,1]), dtype=tf.float32, name='weights')
-b = tf.compat.v1.Variable(tf.compat.v1.zeros([1]), name='bias', dtype=tf.float32)
+w = tf.compat.v1.Variable( tf.compat.v1.random_normal([30,1]) ,dtype=tf.float32 , name='weight'  )
+b = tf.compat.v1.Variable( tf.compat.v1.zeros([1]) ,dtype=tf.float32 , name='bias'  )
 
-print(y_train.shape)
-print(y_test.shape)
+#2 모델
+hypothesis = tf.compat.v1.sigmoid(tf.compat.v1.matmul(Xp,w) + b )
 
-y_train = np.reshape(y_train, (-1, 1))
-y_test = np.reshape(y_test, (-1, 1))
+#3-1 컴파일
+loss = tf.reduce_mean(yp*tf.log(hypothesis)+(1 - yp)*tf.log(1-hypothesis) )
 
-# 2. 모델 구성
-hypothesis = tf.compat.v1.sigmoid(tf.compat.v1.matmul(Xp, w) + b ) 
-
-# 3-1. 컴파일
-loss = -tf.reduce_mean(y * tf.log(hypothesis) + (1 - y) * tf.log(1 - hypothesis))   # 'binary_crossentropy'
-
-
-optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=1e-3)
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
 train = optimizer.minimize(loss)
 
-# 3-2. 훈련
+#3-2 훈련
 sess = tf.compat.v1.Session()
 sess.run(tf.compat.v1.global_variables_initializer())
 
-epochs = 10000
-
-for step in range(epochs):
-    loss_val, _, w_val, b_val = sess.run([loss, train, w, b], feed_dict={Xp: X_train, yp: y_train})
-    if step % 50 ==0:
-        print(step, "loss : ", loss_val)
-        
-# print(w_val, b_val)
-# [[-0.01151588]
-#  [ 0.01308101]
-#  [ 0.12389403]
-#  [-0.12826364]] [-0.0096294]
-
-# print(type(w_val))  # <class 'numpy.ndarray'>
+for step in range(1001) : 
+    _, loss_val , w_val , b_val = sess.run([train , loss , w, b], feed_dict={Xp : X , yp : y})
+    if step % 20==0:
+        print(step , '\t' , loss_val,'\t' , w_val,'\t' , b_val )
 
 
-# 4. 평가, 예측
-test_loss = sess.run(loss, feed_dict={Xp: X_test, yp: y_test})
-print("Test Loss:", test_loss)
+#4 평가
+X_test = tf.compat.v1.placeholder(dtype=tf.float32)
 
-predictions = sess.run(hypothesis, feed_dict={Xp: X_test})
-predicted_classes = np.where(predictions > 0.5, 1, 0)
-print("결과:", predicted_classes)
+y_predict = tf.compat.v1.sigmoid(tf.compat.v1.matmul(X_test,w_val) + b_val)
 
-# 정확도 계산
-acc = accuracy_score(y_test, predicted_classes)
-print("Accuracy:", acc)
+y_pred = sess.run(y_predict , feed_dict={X_test: X})
 
-sess.close()
+y_pred = np.round(y_pred)
 
-# Accuracy: 0.6491228070175439
+acc = accuracy_score(y , y_pred )
+
+print('acc : ',acc)
+
+# acc :  0.8541300527240774
 
