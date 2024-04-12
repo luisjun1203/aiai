d05df754a379717a53349bdb19fbace83b6627b2
luisjun1203 <155038870+luisjun1203@users.noreply.github.com>
2024-04-12 12:53:00 +0900

    Create tf26_gpu_test.py

---
+++ tf114/tf26_gpu_test.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+
+# tf.compat.v1.enable_eager_execution()   # 즉시 실행모드 1.0
+tf.compat.v1.disable_eager_execution()      # 즉시모드 해 2.0
+
+print("텐서플로 버전 : ", tf.__version__)
+print("즉시실행모드 : ", tf.executing_eagerly())
+
+# 텐서플로 버전 :  1.14.0
+# 즉시실행모드 :  True
+
+# 텐서플로 버전 :  1.14.0
+# 즉시실행모드 :  False
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:    
+        tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+        print(gpus[0])
+    except RuntimeError as e:
+        print(e)
+else:
+    print("gpu없다!!!")    
